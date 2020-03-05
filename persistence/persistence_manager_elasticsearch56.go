29eb29f70913121c9736fd5502dd1ab7b7577428
waelchatila <wael.chatila@coupa.com>
2020-03-05 10:35:12 -0800

    foundation go idea sketches

---
+++ persistence/persistence_manager_elasticsearch56.go
@@ -0,0 +1,47 @@
+package persistence
+
+import (
+	"reflect"
+)
+
+var _ PersistenceManager = (*PersistenceManagerElasticSearch56)(nil)
+
+type PersistenceManagerElasticSearch56 struct {
+	Session interface{}
+}
+
+func (self PersistenceManagerElasticSearch56) FindOne(id string) (interface{}, error) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) FindMany(params QueryParams) (interface{}, error) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) CreateOne(obj interface{}) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) DeleteOne(id string) (int, error) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) DeleteMany(params QueryParams) (int, error) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) UpdateOne(id string) (int, error) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) UpdateMany(params QueryParams) (int, error) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) Validate(obj interface{}) (ValidationErrors, error) {
+	panic("implement me")
+}
+
+func (self PersistenceManagerElasticSearch56) GetEntityType() reflect.Type {
+	panic("implement me")
+}
